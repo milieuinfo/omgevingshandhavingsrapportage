61e31f4ba57fed2f968f9b057a05b1126f848e76
Farshad Nour Mohammadi <nourmfa@cronos.be>
2024-06-20 14:20:26 +0200

    hogeraad

--- webapp/src/components/dashboards/ohr-hogeraad.js
+++ webapp/src/components/dashboards/ohr-hogeraad.js
@@ -34,7 +34,6 @@ class OhrHogeRaadDashboard extends LitElement {
       <div style="margin: 3rem 0px">
         <vl-qlik-dashboard-page
             title="Hoge raad voor de handhavingsuitvoering"
-            export-id="JgmbA"
             url="omgevingsloketrapport.omgeving.vlaanderen.be"
             app-id="4202d739-538c-4d7f-8d92-3950359a3602"
             selected-view="Instroom"
@@ -60,11 +59,14 @@ class OhrHogeRaadDashboard extends LitElement {
 Deze pagina geeft via een interactieve tool de cijfers van de opdrachten van de Hoge Raad voor de Handhavingsuitvoering over de jaren heen weer. De cijfers zullen wijzigen naargelang de gekozen filtering.
 Het standaard beeld geeft de totale cijfers van 2021 tot en met het voorgaande jaar weer voor de Hoge Raad voor de Handhavingsuitvoering in heel Vlaanderen.
         </p>
-        <vl-info-tile data-vl-toggleable>
-        <span slot="title">Informatie over het gebruik van het dashboard</span>
-        <div slot="content">Via de filters kan u kiezen om de cijfers van één jaar of één aanvrager weer te geven.
-        De gekozen filtering kan op elk moment worden verwijderd aan de hand van de selectiebalk hieronder. Wanneer de gegevens niet gekend zijn, verschijnt “-“ of wordt aangegeven dat de data niet beschikbaar zijn.</div>
-      </vl-info-tile>
+          <vl-accordion data-vl-toggle-text="Informatie over het gebruik van het dashboard">
+          <span>
+          Het standaard beeld geeft de totale cijfers van 2021 tot en met het voorgaande jaar weer voor de omgevingsinspectie in heel Vlaanderen. <br>
+          Via de filters kan u kiezen om de cijfers van één jaar,  één beleidsniveau of één actor weer te geven.
+          De gekozen filtering kan op elk moment worden verwijderd aan de hand van de selectiebalk hieronder. Wanneer de gegevens niet gekend zijn, verschijnt “-“ of wordt aangegeven dat de data niet beschikbaar zijn.<br>
+          De gekozen filtering kan op elk moment worden verwijderd aan de hand van de selectiebalk hieronder. <br>
+          Wanneer de gegevens niet gekend zijn, verschijnt “-“ of wordt aangegeven dat de data niet beschikbaar zijn.</span>
+        </vl-accordion>
       </vl-typography>
     `;
   }
